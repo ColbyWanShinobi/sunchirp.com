8613c238027e74197cba161d8704597698afae50
Colby Butler <colby@owljet.com>
2023-06-12 23:42:11 -0400

    Added first API endpoint

---
+++ api/src/handlers/getGpsFromZip.js
@@ -0,0 +1,46 @@
+"use strict";
+
+//import convert from 'convert-zip-to-gps';
+let convert = require('convert-zip-to-gps');
+
+module.exports.handler = async function getGpsFromZip(event, context) {
+  const { zipCode }  =  event.pathParameters;
+  let statusCode;
+  try {
+    const raw = convert.zipConvert(zipCode);
+    const [ lat, long ] = raw.split(',');
+    
+    if (!lat || !long ) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify(
+          {
+            message: `No GPS coordinates were found for zip code: ${zipCode}`
+          }
+        )
+      };
+    }
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          zipCode,
+          lat,
+          long
+        }
+      )
+    };
+
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: error.message || 'Multiversal Anomaly'
+        }
+      )
+    };
+  }
+}
